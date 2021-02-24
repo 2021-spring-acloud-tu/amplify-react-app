7e4e9c9e33198c8f9db8432b6724c8c9ed822ddd
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-02-24 08:26:06 -0600

    Completed my #project-03...

--- src/GitHubBornOn.js
+++ src/GitHubBornOn.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React
+    ,{
+        useState
+        , useEffect
+    } from 'react';
+
+import { API } from 'aws-amplify';
 
 export const GitHubBornOn = () => {
 
+    const fetchBornInfo = async () => {
+        try {
+            const data = await API.get("apif65452a3", "/born");
+            updateBornInfo(data.borninfo);
+        }
+        catch(err) {
+            console.error(err);
+        }
+    };
+
+    useEffect(
+        () => {
+            fetchBornInfo();
+        }
+        , []
+    );
+
+    const [bornInfo, updateBornInfo] = useState({});
+
     return(
         <h2>
-            my github name goes here - my git hub created_at goes here
+            {bornInfo.login} - {bornInfo.created_at}
         </h2>
     );
 };
